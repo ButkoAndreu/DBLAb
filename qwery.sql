e5273daef095d7222d8244bd03c4703378865b33
Бутко Андрей <44846143+ButkoAndreu@users.noreply.github.com>
2020-05-30 09:54:43 +0300

    Create qwery.sql

---
+++ qwery.sql
@@ -0,0 +1,21 @@
+--вивести регіон та витрачені на авокадо кошти, що була в цьому регіоні.
+select sum(avgprice), region
+from Prices
+INNER JoIN Regions on
+Regions.id = Prices.id
+GROUP BY regions.region;
+--запит 2 - Вивести регіон та % отриманих даних про авокадо з цього регіону відносно решти регіонів.
+SELECT region, 
+ round( COUNT(*) * 100 / ( SELECT COUNT(*) FROM Regions ), 2 ) as PERC
+FROM Avocado
+JOIN Regions 
+ON Avocado.id=Regions.id
+GROUP BY region
+ORDER BY PERC DESC;
+
+--запит 3 - вивести динаміку змін цін по місяцях.
+SELECT month, avg(avgprice)
+FROM Months 
+JOIN Prices
+ON Months.id = Prices.id
+GROUP BY month;
